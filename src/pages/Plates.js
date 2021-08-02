350417a4218d858cc4ff071d1edb13060e5c7b55
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-08-02 16:31:12 -0500

    added-destock-view

--- src/pages/Plates.js
+++ src/pages/Plates.js
@@ -7,14 +7,14 @@ const Plates = () => {
     const { plates } = useContext(PlateContext);
 
     if (!plates.length) {
-        return <h3>No Books Available</h3>
+        return <h3>No Plates Available</h3>
     }
 
     return (
-        <section className="books">
+        <section className="plates">
             {plates.map(({ image: image, id, title }) => (
-                <article key={id} className="book grow shadow-5">
-                    <div className="book-image">
+                <article key={id} className="plate grow shadow-5">
+                    <div className="plate-image">
                         <img src={image} alt={title} />
                     </div>
                     <Link to={`plates/${id}`} className="btn bg-black book-link">ver mas</Link>
