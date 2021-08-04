c1175e7220d57c244f75fac886fee2128e037065
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-08-04 13:44:51 -0500

    added-info

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 const Hero = () => {
     return (
         <section className="hero">
-            <h2>Delicias</h2>
-            <h3>Sabor <br />Identidad </h3>
+            <h2>Martes <br />A <br />Sabados</h2>
+             <h2>11 Am - 6 Pm</h2>
             <Link className="btn" to="/plates">Todos Los Platos</Link>
+            <br/>
+            <h3>1773 Pali Dr El Paso TX</h3>
         </section>
     )
 }
