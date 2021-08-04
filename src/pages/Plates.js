ead126cd992ebf4b4b922e2629303f5a96a390ed
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-08-04 09:20:20 -0500

    added-update

--- src/pages/Plates.js
+++ src/pages/Plates.js
@@ -6,7 +6,7 @@ import { PlateContext } from '../context/plates';
 const Plates = () => {
     const { plates } = useContext(PlateContext);
     const [searchName,setSearchName]= useState('')
-    const [searByCategory,setSearByCategory]= useState('')
+    const [searByCategory,setSearByCategory]= useState('Aperitivos')
     
     if (!plates.length) {
         return <h3>No Plates Available</h3>
@@ -17,13 +17,27 @@ const Plates = () => {
     }
     return (
         <section className="plates">
-    
-            <div className="filter">
+           <div className="filter">
             <label>Encuentra tu Plato</label>
             <input type="text" 
             placeholder="plate"
             onChange={(e) =>setSearchName(e.target.value)}
             />
+            {/* <div className='buttons-div'>
+                <div>
+                <select 
+                onChage = {(e)=> {
+                    const selectCategory = e.target.value
+                    setSearByCategory(selectCategory)
+                }}
+                >
+                <option value= 'aperitivos'>Aperitivos</option>
+                <option value = 'principales'>Principales</option>
+                <option value= 'segundos'>Segundos</option>
+                </select></div>
+               {searByCategory}
+            
+            </div> */}
             </div>
             {plates.filter(item =>{
             if(searchName ==='' ){
